153802d2fc413580450c7165f0e3bed49e2f0748
Baran <Baran.soysuren@gmail.com>
2024-10-04 10:32:02 +0200

    generate getters and setters

--- Lesson02_MockDatabase/src/main/java/com/learnrest/Lesson02_MockDatabase/Employee.java
+++ Lesson02_MockDatabase/src/main/java/com/learnrest/Lesson02_MockDatabase/Employee.java
@@ -7,5 +7,19 @@ public class Employee {
 	
 	private String Name;
 	private int salary;
+	
+	
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
 
 }
