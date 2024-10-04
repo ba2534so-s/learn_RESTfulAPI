b178bc04c02c20912905455c5b99cc06aff869f4
Baran <Baran.soysuren@gmail.com>
2024-10-04 09:34:33 +0200

    add GET annotation

--- Lesson01_FirstRESTfulJerseyProject/src/main/java/com/learnrest/Lesson01_FirstRESTfulJerseyProject/EmployeeListResource.java
+++ Lesson01_FirstRESTfulJerseyProject/src/main/java/com/learnrest/Lesson01_FirstRESTfulJerseyProject/EmployeeListResource.java
@@ -2,12 +2,14 @@ package com.learnrest.Lesson01_FirstRESTfulJerseyProject;
 
 import java.util.List;
 
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 
 @Path("employeeList")
 public class EmployeeListResource {
 	
 	
+	@GET
 	public List<Employee> getEmployees() {
 		
 	}
