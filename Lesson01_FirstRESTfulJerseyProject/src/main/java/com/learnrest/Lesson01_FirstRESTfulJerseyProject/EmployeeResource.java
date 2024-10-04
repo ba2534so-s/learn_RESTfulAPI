079a7ae1f0987665d52b0f53e1cd6855cbb32b7d
Baran <Baran.soysuren@gmail.com>
2024-10-04 09:11:45 +0200

    add produces annotation

--- Lesson01_FirstRESTfulJerseyProject/src/main/java/com/learnrest/Lesson01_FirstRESTfulJerseyProject/EmployeeResource.java
+++ Lesson01_FirstRESTfulJerseyProject/src/main/java/com/learnrest/Lesson01_FirstRESTfulJerseyProject/EmployeeResource.java
@@ -2,12 +2,14 @@ package com.learnrest.Lesson01_FirstRESTfulJerseyProject;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 
 @Path("employees")
 public class EmployeeResource {
 	
 	
 	@GET
+	@Produces
 	public Employee getEmployee() {
 		System.out.println("getEmployee is called.");
 		
