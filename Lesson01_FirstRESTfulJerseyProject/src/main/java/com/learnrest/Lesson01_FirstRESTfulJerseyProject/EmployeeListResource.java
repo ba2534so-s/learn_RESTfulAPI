57657df6612ad6167412a762fa9c643a6ebbe8d7
Baran <Baran.soysuren@gmail.com>
2024-10-04 09:34:52 +0200

    add produces annotation

--- Lesson01_FirstRESTfulJerseyProject/src/main/java/com/learnrest/Lesson01_FirstRESTfulJerseyProject/EmployeeListResource.java
+++ Lesson01_FirstRESTfulJerseyProject/src/main/java/com/learnrest/Lesson01_FirstRESTfulJerseyProject/EmployeeListResource.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 
 @Path("employeeList")
 public class EmployeeListResource {
 	
 	
 	@GET
+	@Produces
 	public List<Employee> getEmployees() {
 		
 	}
