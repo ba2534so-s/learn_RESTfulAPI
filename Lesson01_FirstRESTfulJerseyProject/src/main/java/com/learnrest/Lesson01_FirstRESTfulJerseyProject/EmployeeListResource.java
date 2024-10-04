2c0f83f8a88634dc24c8b546f24f41f0c3397a6a
Baran <Baran.soysuren@gmail.com>
2024-10-04 09:34:17 +0200

    create getEmployees method signature

--- Lesson01_FirstRESTfulJerseyProject/src/main/java/com/learnrest/Lesson01_FirstRESTfulJerseyProject/EmployeeListResource.java
+++ Lesson01_FirstRESTfulJerseyProject/src/main/java/com/learnrest/Lesson01_FirstRESTfulJerseyProject/EmployeeListResource.java
@@ -1,8 +1,15 @@
 package com.learnrest.Lesson01_FirstRESTfulJerseyProject;
 
+import java.util.List;
+
 import jakarta.ws.rs.Path;
 
 @Path("employeeList")
 public class EmployeeListResource {
+	
+	
+	public List<Employee> getEmployees() {
+		
+	}
 
 }
