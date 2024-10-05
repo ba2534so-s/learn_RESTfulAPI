8b8dc74266c6c4c7c91c0c631d5c1d4449f4693d
Baran <Baran.soysuren@gmail.com>
2024-10-05 11:40:40 +0200

    create get Employees method signature

--- Lesson02_MockDatabase/src/main/java/com/learnrest/Lesson02_MockDatabase/EmployeeResource.java
+++ Lesson02_MockDatabase/src/main/java/com/learnrest/Lesson02_MockDatabase/EmployeeResource.java
@@ -1,10 +1,16 @@
 package com.learnrest.Lesson02_MockDatabase;
 
+import java.util.List;
+
 import jakarta.ws.rs.Path;
 
 @Path("employees")
 public class EmployeeResource {
 	
 	EmployeeRepository repo = new EmployeeRepository();
+	
+	public List<EmployeeRepository> getEmployees() {
+		
+	}
 
 }
