f7b2e08040ef46aa2fe322a0d2ed3179c697c614
Baran <Baran.soysuren@gmail.com>
2024-10-05 23:42:27 +0200

    generate getters and setters

--- Lesson03_PostgresJDBC/src/main/java/com/learnrest/Lesson03_PostgresJDBC/Employee.java
+++ Lesson03_PostgresJDBC/src/main/java/com/learnrest/Lesson03_PostgresJDBC/Employee.java
@@ -8,4 +8,25 @@ public class Employee {
 	private int employeeId;
 	private String name;
 	private int salary;
+	
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	
 }
