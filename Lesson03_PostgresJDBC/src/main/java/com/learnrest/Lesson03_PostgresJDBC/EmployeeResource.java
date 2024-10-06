c00ce409a68fa08683a245937bed8a13354ce3d6
Baran <Baran.soysuren@gmail.com>
2024-10-06 22:44:40 +0200

    add xml and JSON media types

--- Lesson03_PostgresJDBC/src/main/java/com/learnrest/Lesson03_PostgresJDBC/EmployeeResource.java
+++ Lesson03_PostgresJDBC/src/main/java/com/learnrest/Lesson03_PostgresJDBC/EmployeeResource.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("employees")
 public class EmployeeResource {
@@ -11,6 +13,7 @@ public class EmployeeResource {
 	EmployeeRepository repo = new EmployeeRepository();
 	
 	@GET
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	public List<Employee> getEmployees() {
 		
 		
