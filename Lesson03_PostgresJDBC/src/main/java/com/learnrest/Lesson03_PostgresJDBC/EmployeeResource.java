0cd56c0ccc9632b9e8aae317431e8302e6ee76b5
Baran <Baran.soysuren@gmail.com>
2024-10-06 22:48:37 +0200

    add path and path param annotations

--- Lesson03_PostgresJDBC/src/main/java/com/learnrest/Lesson03_PostgresJDBC/EmployeeResource.java
+++ Lesson03_PostgresJDBC/src/main/java/com/learnrest/Lesson03_PostgresJDBC/EmployeeResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
@@ -21,11 +22,14 @@ public class EmployeeResource {
 	
 	
 	@GET
+	@Path("employee/{id}")
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-	public Employee getEmployee(int id) {
+	public Employee getEmployee(@PathParam("id") int id) {
 		return repo.getEmployee(id);
 		
 	}
+	
+	
 	
 	
 
