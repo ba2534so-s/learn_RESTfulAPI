592c5c91b96dba923c8033f067ef7de9092f5b07
Baran <Baran.soysuren@gmail.com>
2024-10-06 22:43:08 +0200

    create getEmloyees method signature

--- Lesson03_PostgresJDBC/src/main/java/com/learnrest/Lesson03_PostgresJDBC/EmployeeResource.java
+++ Lesson03_PostgresJDBC/src/main/java/com/learnrest/Lesson03_PostgresJDBC/EmployeeResource.java
@@ -1,10 +1,19 @@
 package com.learnrest.Lesson03_PostgresJDBC;
 
+import java.util.List;
+
 import jakarta.ws.rs.Path;
 
 @Path("employees")
 public class EmployeeResource {
 	
 	EmployeeRepository repo = new EmployeeRepository();
+	
+	
+	public List<Employee> getEmployees() {
+		
+	}
+	
+	
 
 }
