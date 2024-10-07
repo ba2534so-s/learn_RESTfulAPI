57d467dbe2356c808baf3e59bde9fb41b85bbe85
Baran <Baran.soysuren@gmail.com>
2024-10-07 17:00:03 +0200

    create sample employees and return as list

--- Lesson04_SpringBootREST/src/main/java/com/learnrest/EmployeeResource.java
+++ Lesson04_SpringBootREST/src/main/java/com/learnrest/EmployeeResource.java
@@ -1,13 +1,27 @@
 package com.learnrest;
 
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class EmployeeResource {
 	
 	
 	public List<Employee> getEmployees() {
+		
+		Employee e1 = new Employee();
+		e1.setName("Mary Jane Johnson");
+		e1.setSalary(42000);
+		
+		Employee e2 = new Employee();
+		e2.setName("John Smith");
+		e2.setSalary(37000);
+		
+		List<Employee> employees = Arrays.asList(e1, e2);
+		
+		return employees;
 		
 	}
 
