4cc68a5560741f91f6c606ef15e64d381b012564
Baran <Baran.soysuren@gmail.com>
2024-10-07 16:58:57 +0200

    create getEmployees method signature

--- Lesson04_SpringBootREST/src/main/java/com/learnrest/EmployeeResource.java
+++ Lesson04_SpringBootREST/src/main/java/com/learnrest/EmployeeResource.java
@@ -1,8 +1,14 @@
 package com.learnrest;
 
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Arrays;
 
 @RestController
 public class EmployeeResource {
+	
+	
+	public List<Employee> getEmployees() {
+		
+	}
 
 }
