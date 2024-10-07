dd65ab645636b5b6ae1b56bf697de961e77d782d
Baran <Baran.soysuren@gmail.com>
2024-10-07 17:37:39 +0200

    define data types

--- Lesson04_SpringBootREST/src/main/java/com/learnrest/EmployeeRepository.java
+++ Lesson04_SpringBootREST/src/main/java/com/learnrest/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.learnrest;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface EmployeeRepository extends CrudRepository {
+public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+	
+	
 
 }
