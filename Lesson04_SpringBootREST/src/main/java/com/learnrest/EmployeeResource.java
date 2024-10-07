18b2f22b4413e5a51fcf2502e361d26854eca21e
Baran <Baran.soysuren@gmail.com>
2024-10-07 17:22:00 +0200

    change annotation to GetMapping

--- Lesson04_SpringBootREST/src/main/java/com/learnrest/EmployeeResource.java
+++ Lesson04_SpringBootREST/src/main/java/com/learnrest/EmployeeResource.java
@@ -4,15 +4,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.ArrayList;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 public class EmployeeResource {
 	
-	@RequestMapping("employee")
+	@GetMapping("employees")
 	public List<Employee> getEmployees() {
 		
 		List<Employee> employees = new ArrayList<>();
