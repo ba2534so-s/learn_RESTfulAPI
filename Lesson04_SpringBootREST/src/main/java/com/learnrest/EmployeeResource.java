5780a494e938c0aac0abdc581f58edea2c7b41b5
Baran <Baran.soysuren@gmail.com>
2024-10-07 17:41:44 +0200

    adjust getEmployees to use rest jpa

--- Lesson04_SpringBootREST/src/main/java/com/learnrest/EmployeeResource.java
+++ Lesson04_SpringBootREST/src/main/java/com/learnrest/EmployeeResource.java
@@ -19,20 +19,7 @@ public class EmployeeResource {
 	@GetMapping("employees")
 	public List<Employee> getEmployees() {
 		
-		List<Employee> employees = new ArrayList<>();
-		
-		Employee e1 = new Employee();
-		e1.setEmployeeId(1);
-		e1.setName("Mary Jane Johnson");
-		e1.setSalary(42000);
-		
-		Employee e2 = new Employee();
-		e2.setEmployeeId(2);
-		e2.setName("John Smith");
-		e2.setSalary(37000);
-		
-		employees.add(e1);
-		employees.add(e2);
+		List<Employee> employees = (List<Employee>) repo.findAll();
 		
 		return employees;
 		
