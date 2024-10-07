87662b2bd07bd2b55e495d63ec04e22fdf7b1f5a
Baran <Baran.soysuren@gmail.com>
2024-10-07 17:03:10 +0200

    set ids

--- Lesson04_SpringBootREST/src/main/java/com/learnrest/EmployeeResource.java
+++ Lesson04_SpringBootREST/src/main/java/com/learnrest/EmployeeResource.java
@@ -14,10 +14,12 @@ public class EmployeeResource {
 		List<Employee> employees = new ArrayList<>();
 		
 		Employee e1 = new Employee();
+		e1.setEmployeeId(1);
 		e1.setName("Mary Jane Johnson");
 		e1.setSalary(42000);
 		
 		Employee e2 = new Employee();
+		e2.setEmployeeId(2);
 		e2.setName("John Smith");
 		e2.setSalary(37000);
 		
