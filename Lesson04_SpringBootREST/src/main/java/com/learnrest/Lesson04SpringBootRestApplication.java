816b85435ea603cd438a7b8a7faecbbd621f828d
Baran <Baran.soysuren@gmail.com>
2024-10-07 16:45:17 +0200

    Create Spring Boot starter project for learning REST in SPring Boot

---
+++ Lesson04_SpringBootREST/src/main/java/com/learnrest/Lesson04SpringBootRestApplication.java
@@ -0,0 +1,13 @@
+package com.learnrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson04SpringBootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson04SpringBootRestApplication.class, args);
+	}
+
+}
---
+++ Lesson04_SpringBootREST/src/test/java/com/learnrest/Lesson04SpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson04SpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
